394e0415f5fe1c5764522648fa3c538b4120125b
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-10-27 00:49:15 +0530

    feat: add rect

--- src/main.c
+++ src/main.c
@@ -1,13 +1,26 @@
 #include "raylib.h"
+#include <netdb.h>
+#include <netinet/in.h>
 
 int main(void)
 {
     InitWindow(912, 912, "zao shang hao");
 
+    int x = 100;
+    int y = 100;
+
+    TraceLog(LOG_INFO, "Connecting to server");
+
+    SetTargetFPS(30);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground((Color){0, 0, 0, 0});
-            DrawText("rent free", 100, 100, 40, RAYWHITE);
+            DrawRectangle(x, y, 50, 50, RED);
+            if (IsKeyDown(KEY_W)) y -= 10;
+            if (IsKeyDown(KEY_A)) x -= 10;
+            if (IsKeyDown(KEY_S)) y += 10;
+            if (IsKeyDown(KEY_D)) x += 10;
         EndDrawing();
     }
 
