a8a0be144d828b5f5fc1d876fd2169a1a6d46bcc
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-10-27 04:58:04 +0530

    feat: add update and remove client methods

--- src/draw_clients.c
+++ src/draw_clients.c
@@ -1,20 +1,47 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include "raylib.h"
 
 typedef struct client {
   struct client *next;
+  int id;
   int posX;
   int posY;
 } Client;
 
 Client root = {0};
 
-void add_client(int x, int y) {
+void add_client(int id, int x, int y) {
   Client *c = calloc(1, sizeof(Client));
+  c->id   = id;
   c->posX = x;
   c->posY = y;
   c->next = root.next;
   root.next = c;
+}
+
+bool update_client(int id, int x, int y) {
+  for (Client *curr = root.next; curr != NULL; curr = curr->next) {
+    if (curr->id == id) {
+      curr->posX = x;
+      curr->posY = y;
+      return true;
+    } 
+  }
+  return false;
+}
+
+bool remove_client(int id) {
+  Client *prev = &root;
+  for (Client *curr = root.next; curr != NULL; curr = curr->next) {
+    if (curr->id == id) {
+      prev->next = curr->next;
+      free(curr);
+      return true;
+    } 
+    prev = curr;
+  }
+  return false;
 }
 
 void free_clients(Client *curr) {
